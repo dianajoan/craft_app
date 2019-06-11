261828c605722fdd15660ed1af65a4877ca14cc0
dianajoan <dianajoanita900@gmail.com>
2019-06-11 20:11:13 +0300

    Modified Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,64 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+<div class="signupform">
+    <div class="container">
+        <!-- main content -->
+        <div class="agile_info">
+            <div class="w3l_form">
+                <div class="left_grid_info">
+                    <h1>Manage Your Business Account</h1>
+                    <p>Simply create your crafty profile.</p>
+                    <img src="{!! asset('images/image.jpg') !!}" alt="" />
+                </div>
+            </div>
+            <div class="w3_info">
+                <h2>{{ __('Login') }}</h2>
+                <p>Enter your details to login.</p>
+                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @csrf
+                    <label>{{ __('E-Mail Address') }}</label>
+                    <div class="input-group">
+                        <span class="fa fa-envelope" aria-hidden="true"></span>
+                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Enter Your Email" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                    </div>
+                    <label>{{ __('Password') }}</label>
+                    <div class="input-group">
+                        <span class="fa fa-lock" aria-hidden="true"></span>
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Enter Password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div> 
+                    <div class="login-check">
+                         <label class="checkbox">
+                            <input class="form-check-input" type="checkbox" name="checkbox" checked="" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <i> </i> {{ __('Remember Me') }}</label>
+                    </div>                      
+                        <button class="btn btn-danger btn-block" type="submit">{{ __('Login') }}</button >
+                        <a class="btn btn-link" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>                
+                </form>
+                <p class="account">By clicking login, you agree to our <a href="#">Terms & Conditions!</a></p>
+                <p class="account1">Dont have an account? <a href="#">Register here</a></p>
             </div>
         </div>
+        <!-- //main content -->
     </div>
+    <!-- footer -->
+    <div class="footer">
+        <p>&copy; 2019 Learning DIY Craft. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="blank">Diana Joanita Nakyazze</a></p>
+    </div>
+    <!-- footer -->
 </div>
+
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,26 +1,35 @@
-<html>
-	<head>
-		<title>Learning DIY Craft</title>
-		<link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-        <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
-        <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.css') !!}">
-        <link rel="stylesheet" href="{!! asset('css/bootstrap.css') !!}">
-        
-	</head>
-	<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Learning DIY Craft</title>
+ <!-- Meta-Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Business Login Form a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
 
-	@include('layouts.shared.nav')	
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //Meta-Tags -->
+	
+	<!-- css files -->
+	<link href="{!! asset('css/fonts-awesome.min.css') !!}" rel="stylesheet" type="text/css" media="all">
+	<link href="{!! asset('css/styles.css') !!}" rel="stylesheet" type="text/css" media="all"/>
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	<!-- //google fonts -->
+	
+</head>
+<body>
 
 	@yield('content')
-
-	<script src="{!! asset('js/bootstrap.min.js') !!}"></script>
-	<script src="{!! asset('js/npm.js') !!}"></script>
-	<script src="{!! asset('js/bootstrap.js') !!}"></script>
-	<script>
-		$(document).ready(function() {
-			// This command is used to initialize some elements and make them work properly $.material.init();
-		});
-	</script>
 
 	</body>
 </html>
