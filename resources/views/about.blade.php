a0a63ccc5958b1eefaf80c1f5d95782db2522d4a
dianajoan <dianajoanita900@gmail.com>
2019-06-11 16:11:24 +0300

    Added services in About page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -25,11 +25,8 @@
                 <div class="col-lg-5 about-text-grid pr-xl-5">
                     <h3 class="main-title-w3pvt text-uppercase">First we imagine, then we do everything else.</h3>
                     <hr>
-                    <p class="mt-4">Nam venenatis neque tellus, vel sagittis lacus placerat sit amet. Morbi tristique
-                        consequat mi,
-                        vitae interdum sapien posuere ut.</p>
-                    <p class="mt-3">Mauris lectus ex, rutrum sed gravida eu, Nam augue tortor, pellentesque ut viverra
-                        id.</p>
+                    <p class="mt-4">These are a most of them which are used by many people to create a blanket, sweaters or scalfs as well.</p>
+                    <p class="mt-3">These are a most of them which are used by many people to create a blanket, sweaters or scalfs as well.</p>
                     <a href="{{ route('about') }}" class="button-style-w3ls btn mt-sm-5 mt-4">View More</a>
                 </div>
                 <div class="col-lg-3 col-sm-5 about-right mt-lg-0 mt-5">
@@ -42,42 +39,6 @@
         </div>
     </section>
     <!-- //about -->
-
-    <!-- services -->
-    <div class="serives-w3pvt-web py-5" id="services">
-        <div class="container py-xl-5 py-lg-3">
-            <div class="row support-bottom text-center py-5">
-                <div class="col-lg-4">
-                    <div class="services-w3ls-grid">
-                        <div class="serv-icon mx-auto">
-                            <span class="fa fa-cutlery"></span>
-                        </div>
-                        <h4 class="text-wh mt-md-4 mt-3 mb-3">Service 1</h4>
-                        <p>Ut enim ad minima veniam, quis nostrum ullam corporis suscipit laboriosam.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-lg-0 mt-4">
-                    <div class="services-w3ls-grid">
-                        <div class="serv-icon clr-2 mx-auto">
-                            <span class="fa fa-apple"></span>
-                        </div>
-                        <h4 class="text-wh mt-md-4 mt-3 mb-3">Service 2</h4>
-                        <p>Ut enim ad minima veniam, quis nostrum ullam corporis suscipit laboriosam.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 mt-lg-0 mt-4">
-                    <div class="services-w3ls-grid">
-                        <div class="serv-icon clr-3 mx-auto">
-                            <span class="fa fa-beer"></span>
-                        </div>
-                        <h4 class="text-wh mt-md-4 mt-3 mb-3">Service 3</h4>
-                        <p>Ut enim ad minima veniam, quis nostrum ullam corporis suscipit laboriosam.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- //services -->
 
     <!-- team -->
     <div class="team text-center py-5">
