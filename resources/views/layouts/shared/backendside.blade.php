93b4b3b6855f86bbe8d4c3daa9fe0d20a9612153
dianajoan <dianajoan900@gmail.com>
2019-09-02 20:02:02 +0300

    Removed some links in the sidebar page

--- resources/views/layouts/shared/backendside.blade.php
+++ resources/views/layouts/shared/backendside.blade.php
@@ -3,25 +3,6 @@
 		    <div class="menu">
 		      <ul id="menu" >
 		        <li id="menu-home" ><a href="{{ url('/admin') }}"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
-		        <li><a href="#"><i class="fa fa-cogs"></i><span>Manage Users</span><span class="fa fa-angle-right" style="float: right"></span></a>
-		          <ul>
-		            <li><a href="{{ route('users.index') }}">All</a></li>
-		            <li><a href="{{ route('users.create') }}">Create User</a></li>		            
-		          </ul>
-		        </li>
-		        <li id="menu-comunicacao" ><a href="#"><i class="fa fa-book nav_icon"></i><span>Manage Roles</span><span class="fa fa-angle-right" style="float: right"></span></a>
-		          <ul id="menu-comunicacao-sub" >
-		            <li id="menu-mensagens" style="width: 120px" ><a href="{{ route('roles.index') }}">All</a>		              
-		            </li>
-		            <li id="menu-arquivos" ><a href="{{ route('roles.create') }}">Create Role</a></li>
-		          </ul>
-		        </li>
-		        <li id="menu-academico" ><a href="#"><i class="fa fa-file-text"></i><span>Manage Products</span><span class="fa fa-angle-right" style="float: right"></span></a>
-		          <ul id="menu-academico-sub" >
-		          	 <li id="menu-academico-boletim" ><a href="login.html">All</a></li>
-		            <li id="menu-academico-avaliacoes" ><a href="signup.html">Create a Product</a></li>		           
-		          </ul>
-		        </li>
 		         <li><a href="#"><i class="fa fa-shopping-cart"></i><span>Order Management</span><span class="fa fa-angle-right" style="float: right"></span></a>
 		         	<ul id="menu-academico-sub" >
 			            <li id="menu-academico-avaliacoes" ><a href="product.html">Todays order</a></li>
