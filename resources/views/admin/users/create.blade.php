b2e969e2c82f7ab13b8aa04e79e73c42135fcd88
dianajoan <dianajoan900@gmail.com>
2019-09-06 10:26:10 +0300

    adding more content in the form and functionalities

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -11,71 +11,123 @@
                 <li class="breadcrumb-item active"> Add User </li>
             </ol>
         </div>
-    	<h2>Users Details</h2>   	 
+    	<h2>New User Profile</h2>   	 
 	 	<div class="col-md-13 compose-right">
 			<div class="inbox-details-default">
 				<div class="alert alert-info inbox-details-heading">
-					Creating
+					Fill in the details
 				</div>
 				<div class="inbox-details-body">
-
-					<!-- will be used to show any messages -->
-					@if(Session::has('success'))
-	                    <div class="row">
-	                        <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
-	                            <div id="message" class="alert alert-success">
-	                                {{ Session::get('success') }}
-	                            </div>
-	                        </div>
-	                    </div>
-	                @endif
-					
-					@if ($errors->any())
-					    <div class="alert alert-danger">
-					        <ul>
-					            @foreach ($errors->all() as $error)
-					                <li>{{ $error }}</li>
-					            @endforeach
-					        </ul>
-					    </div>
-					@endif
-
 					<form class="com-mail" method="post" action="{{ route('users.store') }}" accept-charset="UTF-8">
 						
 						@csrf
 
+						<!-- will be used to show any messages -->
+						@if(Session::has('success'))
+		                    <div class="row">
+		                        <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
+		                            <div id="message" class="alert alert-success">
+		                                {{ Session::get('success') }}
+		                            </div>
+		                        </div>
+		                    </div>
+		                @endif
+						
+						@if ($errors->any())
+						    <div class="alert alert-danger">
+						        <ul>
+						            @foreach ($errors->all() as $error)
+						                <li>{{ $error }}</li>
+						            @endforeach
+						        </ul>
+						    </div>
+						@endif
+
+					<input type="hidden" name="_token" value="{!! csrf_token() !!}">
+
 						<div class="form-group">    
-			              <label for="name">Name</label>
-			              <input type="text" class="form-control" id="name" name="name"/>
+			              <label for="name">Full Name</label>
+			              <input type="text" class="form-control" name="name" id="Names" autofocus required/>
 			          	</div>
 
 				        <div class="form-group">
-				            <label for="email">Email</label>
-				            <input type="text" class="form-control" name="email"/>
+				            <label for="email">Email Address</label>
+				            <input type="text" class="form-control" name="email" id="emailAddress1" required/>
 				        </div>
 
 				        <div class="form-group">
-							<label for="select">Role</label>
-								<select class="form-control" id="role" name="role[]" multiple>
-									@foreach($roles as $role)
-										<option value="{!! $role->id !!}">
-											{!! $role->display_name !!}
-										</option>
-									@endforeach
+				            <label for="phoneNumber1">Phone number</label>
+				            <input type="tel" class="form-control" name="telephone" id="phoneNumber1" required/>
+				        </div>
+
+				        <input type="hidden" name="password" value="{{ bcrypt('dollar') }}">
+
+				        <div class="form-group">
+							<label for="gender1">Gender</label>
+								<select class="form-control" id="gender1" name="gender">
+									<option value="Male">Male</option>
+                                    <option value="Female">Female</option>
 								</select>
 						</div>
 
-				        <div class="form-group">
-							<label for="password">Password</label>
-							<input type="password" class="form-control" name="password"/>
-						</div>
-
 						<div class="form-group">
-							<label for="password">Confirm password</label>
-							<input type="password" class="form-control" name="password_confirmation"/>
+				            <label for="date1">Date of Birth</label>
+				            <input type="date" class="form-control" name="date_of_birth" id="date1"/>
+				        </div>
+
+				        <div class="form-group">
+				            <label for="jobTitle1">Occupation</label>
+				            <input type="text" class="form-control" name="occupation" id="jobTitle1"/>
+				        </div>
+
+				        <div class="form-group">
+				            <label for="location1">Location</label>
+				            <input type="text" class="form-control" name="location" class="form-control" id="location1"/>
+				        </div>
+
+				        <div class="form-group">
+				            <label for="nationality1">Nationality</label>
+				            <input type="text" class="form-control" name="nationality" id="nationality1"/>
+				        </div>
+
+				        <div class="form-group">
+							<label for="role1">Role</label>
+								<select class="form-control" id="role1" name="role" required>
+									@foreach($roles as $role)
+                                        <option value="{{ $role->name }}" title="{{ $role->description }}">{{ $role->display_name }}
+                                        </option>
+                                    @endforeach
+								</select>
 						</div>
 
-				        <button type="submit" class="btn btn-primary-outline">Add</button>
+						<input type="hidden" name="level" value="1">
+
+				        <div class="form-group">
+                            <label>Account Status :</label>
+                            <div class="c-inputs-stacked">
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadio1" name="status" value="Active" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio1">Active </label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadio2" name="status" value="Busy" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio2">Busy</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadio4" name="status" value="Blocked" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio4">Blocked</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadio5" name="status" value="Away" class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio5">Away</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="customRadio6" name="status" value="Pending" class="custom-control-input" checked="checked">
+                                    <label class="custom-control-label" for="customRadio6">Pending</label>
+                                </div>
+                            </div>
+                        </div>
+				        <button type="submit" class="btn btn-rounded btn-success">Add</button>
 				        <button type="reset" class="btn btn-primary-outline">Cancel</button>
 				      </form>
 					</div>
