5b65721e95a78d249b31354ba6a5f78fd24bb645
dianajoan <dianajoan900@gmail.com>
2019-09-11 21:42:25 +0300

    Added a favicon icon for the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
   <title>Welcome | {{ config('app.name') }}</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon.ico') }}">
 </head>
 <body>
 <!-- partial:index.partial.html -->
