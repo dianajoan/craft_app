02cc2c1e35392b538158c3c58ac22ba5d1c06fee
dianajoan <dianajoan900@gmail.com>
2019-09-20 09:42:13 +0300

    modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon.ico') }}">
-</head>
+</head> 
 <body>
 <!-- partial:index.partial.html -->
 <h1>Welcome <strong>to the Craft</strong></h1>
